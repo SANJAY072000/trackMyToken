b93a32ae73708f856f39b3272bcbf5dd91e4428a
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-07-29 18:51:57 +0530

    Done with Banner design

---
+++ client/src/components/home/Ap.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+export default class Ap extends Component {
+  render(){
+    return(
+      <aside classname="mt-5">
+      </aside>
+    );
+  }
+}
--- client/src/components/home/Welcome.js
+++ client/src/components/home/Welcome.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
 import Banner from './Banner';
+import Feature from './Feature';
+import Ap from './Ap';
 
 export default class Welcome extends Component {
   render(){
     return(
       <div>
       <Banner/>
+      <Feature/>
+      <Ap/>
       </div>
     );
   }
