1859ef14535e5d8d5c813b61aeeb63cb31185171
SANJAY SINGH BISHT <39243846+SANJAY072000@users.noreply.github.com>
2019-09-12 20:24:12 +0530

    Updated setup

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,3 @@
 // exporting the configuration of backend
 module.exports={
-    mongoUrl:'mongodb+srv://sanjay_20:san07jay@cluster0-yxg19.mongodb.net/test?retryWrites=true&w=majority',
-    secret:'Kick your ass'
 };
